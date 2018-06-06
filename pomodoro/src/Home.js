3b7659f8b079dadeeadf0fba58cd69f2f57a2432
Christian Lessard <christian@d-172-27-79-76.dhcp.virginia.edu>
2018-06-06 13:53:30 -0400

    authentication works

---
+++ pomodoro/src/Home.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import { Button } from 'react-bootstrap';
+import fire from './fire';
+
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.logout = this.logout.bind(this);
+  }
+
+  logout() {
+    fire.auth().signOut();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>You Are Home</h1>
+        <Button className="btn btn-warning" onClick={this.logout}>Logout</Button>
+      </div>
+
+    )
+  }
+}
