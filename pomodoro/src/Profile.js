4ae2dad376ed2f2358d114e226f057ec81503e56
Taylor Rauch <Taylor@d-172-25-79-93.dhcp.virginia.edu>
2018-06-06 11:27:34 -0400

    connect drawer and profile page

--- pomodoro/src/Profile.js
+++ pomodoro/src/Profile.js
@@ -1,92 +1,95 @@
-import React, { Component } from 'react';
-import { AppBar, Toolbar, Card, CardContent, Typography, IconButton } from "@material-ui/core";
+import React, { Component } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Card,
+  CardContent,
+  Typography,
+  IconButton
+} from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 
 export default class Profile extends React.Component {
-
-    constructor(props) {
+  constructor(props) {
     super(props);
     this.state = {
-     name: "",
-     github: "",
-     activitylog: [],
-     numCycles: 0,
-     totalCycles: 0,
-     totalTime: 0
-   }
-}
+      name: "",
+      github: "",
+      activitylog: [],
+      numCycles: 0,
+      totalCycles: 0,
+      totalTime: 0
+    };
+  }
 
-render () {
+  render() {
     return (
-        
-    <div>
+      <div>
         <AppBar>
-        <Toolbar>
-            <IconButton color="inherit" style={{marginLeft: -12, marginRight: 20}}>
-                <MenuIcon/>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              style={{ marginLeft: -12, marginRight: 20 }}
+              onClick={e => this.props.updateParent(true)}
+            >
+              <MenuIcon />
             </IconButton>
-        Profile Page
-        </Toolbar>
+            Profile Page
+          </Toolbar>
         </AppBar>
 
         <Card
-        style={{
+          style={{
             marginLeft: 20,
             marginRight: 20,
             marginTop: 80,
             marginBottom: 20
-        }}
+          }}
         >
-        <CardContent>
+          <CardContent>
             <Typography>
-                <strong>Name: {this.state.name} </strong>
+              <strong>Name: {this.state.name} </strong>
             </Typography>
             <Typography>
-                <strong>GitHub: {this.state.github} </strong>
+              <strong>GitHub: {this.state.github} </strong>
             </Typography>
-        </CardContent>
-            
+          </CardContent>
         </Card>
         <Card
-        style={{
+          style={{
             marginLeft: 20,
             marginRight: 20,
             marginTop: 20,
             marginBottom: 20
-        }}
+          }}
         >
-        <CardContent>
+          <CardContent>
             <Typography>
-                <strong>Number of Cycles Today: {this.state.numCycles} </strong>
+              <strong>Number of Cycles Today: {this.state.numCycles} </strong>
             </Typography>
             <Typography>
-                <strong>Total Number of Cycles: {this.state.totalCycles} </strong>
+              <strong>Total Number of Cycles: {this.state.totalCycles} </strong>
             </Typography>
             <Typography>
-                <strong>Total Time: {this.state.totalTime} </strong>
+              <strong>Total Time: {this.state.totalTime} </strong>
             </Typography>
-        </CardContent>
-            
+          </CardContent>
         </Card>
-        
+
         <div style={{ overflow: "auto" }}>
-        <Card
-        style={{
-            marginLeft: 20,
-            marginRight: 20,
-            marginTop: 3,
-            marginBottom: 20,
-            height: 500
-        }}
-        >
-        <CardContent>
-        </CardContent>
-            
-        </Card>
+          <Card
+            style={{
+              marginLeft: 20,
+              marginRight: 20,
+              marginTop: 3,
+              marginBottom: 20,
+              height: 500
+            }}
+          >
+            <CardContent />
+          </Card>
         </div>
-    </div>
+      </div>
     );
-}
-
-
+  }
 }
