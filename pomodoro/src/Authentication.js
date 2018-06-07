201338015f4169d01ad3d38833d88a0902a42fbf
Edward Radion <er2cg@virginia.edu>
2018-06-07 11:51:50 -0400

    added logo and cleaned up initial login page

--- pomodoro/src/Authentication.js
+++ pomodoro/src/Authentication.js
@@ -1,84 +1,82 @@
-import React, {Component} from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import { Button, Row, Col, grid } from 'react-bootstrap';
-import SignInForm from './SignInForm';
-import SignUpForm from './SignUpForm';
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import { Button, Row, Col, grid } from "react-bootstrap";
+import SignInForm from "./SignInForm";
+import SignUpForm from "./SignUpForm";
+import logo from "./tomatologo.png";
+import "./Authentication.css";
+import { createMuiTheme } from "@material-ui/core/styles";
+import teal from "@material-ui/core/colors/teal";
 
 export default class Authentication extends Component {
   constructor() {
     super();
     this.state = {
       activeIndex: 0
-    }
+    };
     this.goBack = this.goBack.bind(this);
   }
 
   goBack = () => {
     this.setState({
       activeIndex: 0
-    })
-  }
+    });
+  };
 
   renderContent = () => {
-    if(this.state.activeIndex == 0) {
-      return(
-
-          <Row className="show-grid">
-            <Col xs={6}>
-              <Button bsSize="large" bsStyle="info" onClick={()=> this.onSignUp()}>Sign Up</Button>
-            </Col>
-            <Col xs={6}>
-              <Button bsSize="large" bsStyle="info" onClick={()=> this.onSignIn()}>Sign In</Button>
-            </Col>
-          </Row>
-
-
+    if (this.state.activeIndex == 0) {
+      return (
+        <Row className="show-grid">
+          <Col xs={6}>
+            <Button
+              bsSize="large"
+              bsStyle="info"
+              onClick={() => this.onSignUp()}
+            >
+              Sign Up
+            </Button>
+          </Col>
+          <Col xs={6}>
+            <Button
+              bsSize="large"
+              bsStyle="info"
+              onClick={() => this.onSignIn()}
+            >
+              Sign In
+            </Button>
+          </Col>
+        </Row>
       );
-    } else if(this.state.activeIndex == 1) {
-      return(
-        <SignUpForm goBack={this.goBack} />
-      );
+    } else if (this.state.activeIndex == 1) {
+      return <SignUpForm goBack={this.goBack} />;
     } else {
-      return(
-        <SignInForm goBack={this.goBack}/>
-      );
+      return <SignInForm goBack={this.goBack} />;
     }
-  }
+  };
 
   onSignUp = () => {
     this.setState({
       activeIndex: 1
-    })
-  }
+    });
+  };
 
   onSignIn = () => {
     this.setState({
       activeIndex: 2
-    })
-  }
-
+    });
+  };
 
   render() {
-
-
-    return(
-
-      <Grid>
-          <Row className="show-grid">
-            <Col xs={12} className="text-center" style={{paddingLeft: 300}}>
-              <h1>Pomodoro App</h1>
-              <h4>Brought to you by:</h4>
-              <h2>Team Tomato</h2>
-              <div className="alert alert-primary" role="alert">
-                A simple primary alert—check it out!
-              </div>
-            </Col>
-          </Row>
-
-            {this.renderContent()}
-
-        </Grid>
+    return (
+      <div>
+        <div>
+          <header className="Header">
+            <img src={logo} className="tomato-logo" alt="logo" />
+          </header>
+        </div>
+        {this.renderContent()}
+      </div>
     );
   }
 }
--- pomodoro/src/Timer.js
+++ pomodoro/src/Timer.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import ReactCountdownClock from 'react-countdown-clock'
-
+import React from "react";
+import ReactDom from "react-dom";
+import ReactCountdownClock from "react-countdown-clock";
 
 import {
   AppBar,
@@ -22,7 +21,6 @@ import {
   createMuiTheme
 } from "@material-ui/core/styles";
 
-
 const theme = createMuiTheme({
   palette: {
     primary: green
@@ -31,46 +29,54 @@ const theme = createMuiTheme({
 
 export default class Timer extends React.Component {
   constructor(props) {
-        super(props);
-        this.state = {
-          button_text: "Start working", 
-          paused: true, 
-          activity: "", 
-          time: 5, 
-          status: "Work now!",
-        };
-      }
+    super(props);
+    this.state = {
+      button_text: "Start working",
+      paused: true,
+      activity: "",
+      time: 5,
+      status: "Work now!"
+    };
+  }
 
-    startTimer = e => {
-        e.preventDefault();
-        this.setState ({ paused: !this.state.paused })
+  startTimer = e => {
+    e.preventDefault();
+    this.setState({ paused: !this.state.paused });
+  };
+
+  switchTimes = e => {
+    if (this.state.time === 5) {
+      this.setState({
+        time: 2.5,
+        button_text: "Start break",
+        status: "Take a break!"
+      });
+    } else if (this.state.time === 2.5) {
+      this.setState({
+        time: 5,
+        button_text: "Start working",
+        status: "Work now!"
+      });
     }
+    this.setState({
+      paused: true
+    });
+  };
 
-   switchTimes = e => {
-        if(this.state.time === 5) {
-            this.setState({time: 2.5, button_text: "Start break", status: "Take a break!"})
-        }
-        else if(this.state.time === 2.5) {
-            this.setState({time: 5, button_text: "Start working", status: "Work now!"})
-        }
-        this.setState({
-            paused: true
-        })
-    }
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
 
-    onChange = e => {
-        this.setState({
-            [e.target.name]: e.target.value
-        });
-    }
-
-    render(){
+  render() {
     return (
+      <div>
         <div>
-        <div>
-          <AppBar position="static"
+          <AppBar
+            position="static"
             style={{
-              backgroundColor: "#cc3737"
+              backgroundColor: "darkred"
             }}
           >
             <Toolbar>
@@ -79,28 +85,45 @@ export default class Timer extends React.Component {
                 style={{ marginLeft: -12, marginRight: 20 }}
                 onClick={e => this.props.updateParent(true)}
               >
-              <MenuIcon />
-            </IconButton>
-            Timer
-          </Toolbar>
-        </AppBar>
+                <MenuIcon />
+              </IconButton>
+              Timer
+            </Toolbar>
+          </AppBar>
         </div>
-        <div className = "status" >
-            <h2> {this.state.status} </h2> 
-        </div> 
-        <div className = "Work-timer">
-          <ReactCountdownClock seconds={this.state.time} color="#000" alpha={0.9} size={300} paused={this.state.paused} onComplete={this.switchTimes}/>
+        <div className="status">
+          <h2> {this.state.status} </h2>
         </div>
-        <div className = "Start-button"> 
+        <div className="Work-timer">
+          <ReactCountdownClock
+            seconds={this.state.time}
+            color="#000"
+            alpha={0.9}
+            size={300}
+            paused={this.state.paused}
+            onComplete={this.switchTimes}
+          />
+        </div>
+        <div className="Start-button">
           <MuiThemeProvider theme={theme}>
-            <Button variant="contained" color="primary" onClick={this.startTimer}> {this.state.button_text} </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={this.startTimer}
+            >
+              {" "}
+              {this.state.button_text}{" "}
+            </Button>
           </MuiThemeProvider>
-        </div> 
-        <div className = "Activity-input">
-          <TextField name="activity" placeholder="activity" onChange={this.onChange}/>
         </div>
+        <div className="Activity-input">
+          <TextField
+            name="activity"
+            placeholder="activity"
+            onChange={this.onChange}
+          />
         </div>
-        )
-    }
-
+      </div>
+    );
+  }
 }
